c75ecc15f646712b92b6c2165ec314b6ae40a941
iamomerjaveed <omerjaveedlearning@gmail.com>
2020-01-22 14:42:20 +0300

    deleted files of employee payroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmployeeDComponent } from './employee-d/employee-d.component';
 import { EmpAttendanceComponent } from './emp-attendance/emp-attendance.component';
-import { EmployeePayrollComponent } from './employee-payroll/employee-payroll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeDComponent,
-    EmpAttendanceComponent,
-    EmployeePayrollComponent
+    EmpAttendanceComponent
   ],
   imports: [
     BrowserModule
