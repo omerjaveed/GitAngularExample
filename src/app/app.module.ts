d58e87bbeaf8b819a11f793e8fd30ff61bd50d32
iamomerjaveed <omerjaveedlearning@gmail.com>
2020-01-22 16:39:34 +0300

    Employee Certificate Folder added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { EmployeeDComponent } from './employee-d/employee-d.component';
 import { EmpAttendanceComponent } from './emp-attendance/emp-attendance.component';
 import { EmployeePayrollComponent } from './employee-payroll/employee-payroll.component';
+import { EmployeeCertificateComponent } from './employee-certificate/employee-certificate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeDComponent,
     EmpAttendanceComponent,
-    EmployeePayrollComponent
+    EmployeePayrollComponent,
+    EmployeeCertificateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employee-certificate/employee-certificate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-certificate',
+  templateUrl: './employee-certificate.component.html',
+  styleUrls: ['./employee-certificate.component.css']
+})
+export class EmployeeCertificateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
