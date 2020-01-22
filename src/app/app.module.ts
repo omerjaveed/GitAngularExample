23671fb4c239f23fbf58ddbca2eda94fae2dec0c
iamomerjaveed <omerjaveedlearning@gmail.com>
2020-01-22 14:52:14 +0300

    Revert "Employee Dependent component added"
    This reverts commit 9de9cb9786a9d664d557bedb13eb3f099c9d2646.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmployeeDComponent } from './employee-d/employee-d.component';
 import { EmpAttendanceComponent } from './emp-attendance/emp-attendance.component';
-import { EmployeeDependentComponent } from './employee-dependent/employee-dependent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeDComponent,
-    EmpAttendanceComponent,
-    EmployeeDependentComponent
+    EmpAttendanceComponent
   ],
   imports: [
     BrowserModule
